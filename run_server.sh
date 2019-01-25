e61549e97e8658f0a39d14b053f13cdd7d2145a4
chenfx <chenfx@inspur.com>
2019-01-25 09:46:05 +0800

    update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+init_env()
+{
+    source /usr/local/virtualenvs/venv_devopspy/bin/activate
+    sed -i 's/127.0.0.1/0.0.0.0/g' django/core/management/commands/runserver.py
+}
 
-source /usr/local/virtualenvs/venv_devopspy/bin/activate
-
-sed -i 's/127.0.0.1/0.0.0.0/g' django/core/management/commands/runserver.py
-
-./manage.py runserver
-if [ $? -ne 0 ];then
+init_server()
+{
     pip install -t . -Ur requirements.txt
+    ./manage.py makemigrations
+    ./manage.py migrate
+    sed -i 's/127.0.0.1/0.0.0.0/g' django/core/management/commands/runserver.py
+}
+
+run_server()
+{
     ./manage.py runserver
-fi
+}
+
+init_env
+[ "x$1" = "xinit" ] && init_server
+run_server
