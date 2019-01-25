496a34a897b773f369fd15e8d68ffbdba36c8208
chenfx <chenfx@inspur.com>
2019-01-25 09:06:56 +0800

    update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -3,10 +3,10 @@
 
 source /usr/local/virtualenvs/venv_devopspy/bin/activate
 
-pip install -t . -Ur requirements.txt
-
 sed -i 's/127.0.0.1/0.0.0.0/g' django/core/management/commands/runserver.py
 
 ./manage.py runserver
-
-
+if [ $? -ne 0 ];then
+    pip install -t . -Ur requirements.txt
+    ./manage.py runserver
+fi
